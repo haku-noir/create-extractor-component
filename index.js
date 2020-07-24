567c7ab692af7085ea3b1b0e53a2cf229ea58741
haku-noir <hakunoir.00@gmail.com>
2020-07-25 01:56:29 +0900

    add: srcディレクトリを探す関数の追加

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const getSrcDir = dir => {
+  let srcDir = `${dir}/src`;
+
+  dir.split('/').forEach((dirItem, index) => {
+    if(dirItem === 'src')
+      srcDir = dir.split('/').slice(0, index).join('/') + '/src';
+  });
+
+  return srcDir;
+};
+
 module.exports = () => {
+  const srcDir = getSrcDir(process.cwd());
+
   Promise.resolve()
-   .then(() => console.log('create-extractor-component'))
-   .catch(err => {
-     console.log('Fail!');
-     console.error(err)
-   });
+    .then(() => console.log('create-extractor-component'))
+    .then(() => console.log(srcDir))
+    .catch(err => {
+      console.log('Fail!');
+      console.error(err)
+    });
 };
