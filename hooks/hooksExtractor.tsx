05469e67013ef938f3cecead8a4f2676c8418b36
haku-noir <hakunoir.00@gmail.com>
2020-07-27 02:38:58 +0900

    clean: 改行を挿入

--- hooks/hooksExtractor.tsx
+++ hooks/hooksExtractor.tsx
@@ -3,14 +3,16 @@ import { Dispatch, Action } from 'redux';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from 'store';
 
-const extract = (data: RootState): RootState => {
-  return data;
-}
+const extract = (data: RootState): RootState => (
+  data
+);
 
 export const XXXExtractor: React.FC<{}> = () => {
   const dispatch = useDispatch<Dispatch<Action>>();
 
-  const data = useSelector<RootState, RootState>(state => state);
+  const data = useSelector<RootState, RootState>(
+    state => state
+  );
 
   React.useEffect(() => {
     const extractedData = extract(data);
